bb2032984ef435535358a06cf77efb3ef3a487b2
Said Fekih <saidgwx@gmail.com>
2022-02-28 13:19:23 -0500

    Added bg color direction options

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ let bodyColor = document.querySelector('#gradient');
 let resultColor = document.querySelector('.resultColor');
 let cssprop = document.querySelector('#Cssprop');
 let coppied = document.querySelector('#coppied');
-let modal = document.querySelector(".modal")
+let modal = document.querySelector(".modal");
+let gradientDirectionColor = 'to bottom';
 
 color1.addEventListener('input', generateColor);
 color2.addEventListener('input', generateColor);
@@ -18,13 +19,13 @@ function generateColor() {
     //console.log(resultColor);
     color1ActualValue1.value = color1.value;
     color1ActualValue2.value = color2.value;
-    setBGCorlo(color1ActualValue1.value, color1ActualValue2.value)
+    setBGCorlo('to bottom', color1ActualValue1.value, color1ActualValue2.value)
     clearTimeout(myTimeout);
 }
 
-function setBGCorlo(col1, col2) {
+function setBGCorlo(gradientDirectionColor,col1, col2) {
     //console.log('btn mriguel' + color2.value)
-    let gradientColor = `linear-gradient(to right, ${col1} ,${col2})`;
+    let gradientColor = `linear-gradient(${gradientDirectionColor}, ${col1} ,${col2})`;
     console.log(gradientColor);
     resultColor.style.backgroundImage = gradientColor;
     cssprop.value = " background-Image:" + gradientColor;
@@ -43,4 +44,24 @@ function confirmationMessage(){
 function hideMessage(){
     coppied.setAttribute("style","display: none;"); 
     modal.setAttribute("style","display: none;"); 
+}
+function gradientDirection(typeId){
+    let direction = '';
+    switch(typeId){
+        case '1': direction ='to bottom'
+        break;
+        case '2': direction ='to right'
+        break;
+        case '3': direction ='to bottom right'
+        break;
+        case '4': direction ='360deg'
+        break;
+        case '5': direction ='to left'
+        break;
+        default: direction ='to bottom'
+
+    }
+    color1ActualValue1.value = color1.value;
+    color1ActualValue2.value = color2.value;
+    setBGCorlo(direction,color1ActualValue1.value  ,color1ActualValue2.value);
 }
